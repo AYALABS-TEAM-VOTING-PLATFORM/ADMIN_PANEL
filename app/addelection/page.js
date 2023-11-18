ee03806cba5e45e18f7fc1ddf289a37d9d9667c2
Emmanuel Umeh <emmachinemerem04@gmail.com>
2023-11-18 06:12:01 -0800

    Made_Responsive_On_The_Sidenav

--- app/addelection/page.js
+++ app/addelection/page.js
@@ -1,44 +1,66 @@
-"use client";
-
 import React from "react";
 import DoughnutChart from "@/components/DoughnutChart";
 import Image from "next/image";
-import { BiChevronDown} from "react-icons/bi";
+import { BiChevronDown } from "react-icons/bi";
 
 export default function Dashboard() {
-// const [loading, setLoading] = useState(false)
-
   return (
-    <div className="flex lg:ml-10 justify-center items-center pl-20 pr-[20px] mt-[20px] sm:ml-[0px] md:ml-[0px] sm:pl-7 md:pl-7">
-    
-      {/* second */}
-    
-      <div className="bg-white rounded-md py-5 w-full px-5 sm:w-[100%] md:w-[100%]  pt-10">
-            <h3 className="mb-7 text-3xl font-semibold text-center text-[#121212]">Add Election</h3>
-            <form className="">
-                  {/* first input */}
-                <div className="flex gap-10 mb-10 lg:flex-col sm:flex-col md:flex-col">
-                  <input type="text" placeholder="Enter" className="text-[#121212] px-1 py-2 pr-48 sm:pr-0 md:pr-0 border-2"/>
-                  <input type="text" placeholder="Enter" className="text-[#121212] px-1 py-2 pr-48 border-2"/>
-                </div>
+    <div className="ml-20 md:ml-20 mt-5 sm:ml-0 md:ml-0">
+      <div className=" bg-white rounded-md py-5 w-full px-5 pt-10">
+        <h3 className="mb-7 text-3xl font-semibold text-center text-[#121212]">
+          Add Election
+        </h3>
+        <form className="">
+          {/* first input */}
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5 mb-5">
+            <input
+              type="text"
+              placeholder="Enter"
+              className="text-[#121212] px-3 py-2 border-2"
+            />
+            <input
+              type="text"
+              placeholder="Enter"
+              className="text-[#121212] px-3 py-2 border-2"
+            />
+          </div>
 
-                   {/* second input */}
-                   <div className="flex gap-10 lg:flex-col sm:flex-col mb-10 md:flex-col">
-                  <input type="text" placeholder="Enter" className="py-2 text-[#121212] pr-48 border-2"/>
-                  <input type="text" placeholder="Enter" className=" px-1 py-2 text-[#121212] pr-48 border-2"/>
-                </div>
+          {/* second input */}
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5 mb-5">
+            <input
+              type="text"
+              placeholder="Enter"
+              className="px-3 py-2 text-[#121212] border-2"
+            />
+            <input
+              type="text"
+              placeholder="Enter"
+              className="px-3 py-2 text-[#121212] border-2"
+            />
+          </div>
 
-                   {/* third input */}
-                   <div className="flex gap-10 lg:flex-col sm:flex-col mb-3 md:flex-col">
-                  <input type="text" placeholder="Enter" className=" px-1 py-2 text-[#121212] pr-48 border-2"/>
-                  <input type="text" placeholder="Enter" className=" px-1 py-2 text-[#121212] pr-48 border-2"/>
-                </div>
-              <button type="submit" className="w-full bg-[#5570F1] rounded-lg text-white p-2 px-4 font-semibold mt-5">Submit</button>
-            </form>
+          {/* third input */}
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-5">
+            <input
+              type="text"
+              placeholder="Enter"
+              className="px-3 py-2 text-[#121212] border-2"
+            />
+            <input
+              type="text"
+              placeholder="Enter"
+              className="px-3 py-2 text-[#121212] border-2"
+            />
+          </div>
 
-        </div>
-    
-      
+          <button
+            type="submit"
+            className="w-full bg-[#5570F1] rounded-lg text-white p-2 px-4 font-semibold mt-5"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
