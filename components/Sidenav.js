22ae7551c1927a0e2fcec1e29c68e795809d0890
Emmanuel Umeh <emmachinemerem04@gmail.com>
2023-11-22 11:06:01 -0800

    Resolved_linking_Issue

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -1,80 +1,77 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BiCategoryAlt, BiSelection } from "react-icons/bi";
 import { HiOutlineUsers } from "react-icons/hi";
 import { TbLogout } from "react-icons/tb";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
 const Sidenav = () => {
     const [activeButton, setActiveButton] = useState("dashboard");
-
-    const handleClick = (buttonType) => {
-        setActiveButton(buttonType);
-    };
-
     const path = usePathname();
 
+    useEffect(() => {
+        const pathSegments = path.split("/");
+        const currentButton = pathSegments[1] || "dashboard";
+        setActiveButton(currentButton);
+    }, [path]);
+
     return (
-        <>
-            <div className="fixed overflow:[hidden] top-0 z-50 bg-white w-[296px] pl-7 pb-20 h-full sm:hidden md:hidden xl:block">
+        <div className="fixed overflow:[hidden] top-0 z-50 bg-white w-[296px] pl-7 pb-20 h-full sm:hidden md:hidden xl:block">
+            <div className="pl-10 pt-5 mb-10">
+                <h3 className="text-2xl font-semibold">Ayalabs</h3>
+            </div>
 
-                <div className="pl-10 pt-5 mb-10">
-                    <h3 className="text-2xl font-semibold">Ayalabs</h3>
+            <div>
+                {/* Dashboard */}
+                <div className="pl-10 pt-5">
+                    <Link href="/" onClick={() => setActiveButton("dashboard")}>
+                        <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "dashboard" ? 'bg-[#5570F1] text-white' : ''}`}>
+                            <span className="flex items-center">
+                                <BiCategoryAlt size={25} />
+                                <span>Dashboard</span>
+                            </span>
+                        </button>
+                    </Link>
                 </div>
 
-                <div>
-                    {/* Dashboard */}
-                    <div className="pl-10 pt-5">
-                        <Link href="/" onClick={() => handleClick("dashboard")}>
-                            <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "dashboard" ? 'bg-[#5570F1] text-white' : ''}`}>
-                                <span className="flex items-center">
-                                    <BiCategoryAlt size={25} />
-                                    <span>Dashboard</span>
-                                </span>
-                            </button>
-                        </Link>
-                    </div>
-
-                    {/* Add Election */}
-                    <div className="pl-10 pt-5">
-                        <Link href={`/${path.split("/")[1]}/addelection`} onClick={() => handleClick("addelection")}>
-                            <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "addelection" ? 'bg-[#5570F1] text-white' : ''}`}>
-                                <span className="flex items-center">
-                                    <BiSelection size={25} />
-                                    <span className="ml-2">Add Election</span>
-                                </span>
-                            </button>
-                        </Link>
-                    </div>
-
-                    {/* Add Candidate */}
-                    <div className="pl-10 pt-5">
-                        <Link href={`/${path.split("/")[1]}/addcandidate`} onClick={() => handleClick("addcandidate")}>
-                            <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "addcandidate" ? 'bg-[#5570F1] text-white' : ''}`}>
-                                <span className="flex items-center">
-                                    <HiOutlineUsers size={25} />
-                                    <span className="ml-2 text-sm">Add Candidate</span>
-                                </span>
-                            </button>
-                        </Link>
-                    </div>
+                {/* Add Election */}
+                <div className="pl-10 pt-5">
+                    <Link href="/addelection" onClick={() => setActiveButton("addelection")}>
+                        <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "addelection" ? 'bg-[#5570F1] text-white' : ''}`}>
+                            <span className="flex items-center">
+                                <BiSelection size={25} />
+                                <span className="ml-2">Add Election</span>
+                            </span>
+                        </button>
+                    </Link>
                 </div>
 
-                <div className="pl-7 pt-5 mt-32">
-                    <button className="">
-                        <span className="flex flex-col items-center text-[#53545C]">
-                            <div className="flex mb-4">
-                                <TbLogout size={25} color="#CC5F5F" />
-                                <span className="text-[#CC5F5F] ml-3">Logout</span>
-                            </div>
-                        </span>
-                    </button>
+                {/* Add Candidate */}
+                <div className="pl-10 pt-5">
+                    <Link href="/addcandidate" onClick={() => setActiveButton("addcandidate")}>
+                        <button className={`pr-20 pt-4 pb-4 pl-4 rounded-md ${activeButton === "addcandidate" ? 'bg-[#5570F1] text-white' : ''}`}>
+                            <span className="flex items-center">
+                                <HiOutlineUsers size={25} />
+                                <span className="ml-2 text-sm">Add Candidate</span>
+                            </span>
+                        </button>
+                    </Link>
                 </div>
             </div>
-        </>
+
+            <div className="pl-7 pt-5 mt-32">
+                <button className="">
+                    <span className="flex flex-col items-center text-[#53545C]">
+                        <div className="flex mb-4">
+                            <TbLogout size={25} color="#CC5F5F" />
+                            <span className="text-[#CC5F5F] ml-3">Logout</span>
+                        </div>
+                    </span>
+                </button>
+            </div>
+        </div>
     );
 };
 
